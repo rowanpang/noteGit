c89376ba2eff9b7d28d1a07a03864bf1af487ce5
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-06-15 18:48:44 +0800

    1,add C expression test suit

---
+++ gnu/expression_c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc,char ** argv)
+{
+	int i = 1;
+	while( i = 0){
+		printf("in while 1\n");
+		break;
+	}
+	
+	while( i = 1){
+		printf("in while 2\n");
+		break;
+	}
+
+	return 0;
+}
+
