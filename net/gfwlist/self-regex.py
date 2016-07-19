d4915933287e0c1a39968cce80d85f2e146de2ff
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-07-19 11:36:28 +0800

    1,gfwlist to squid domain, TODO:need optimize

--- net/gfwlist/self-regex.py
+++ net/gfwlist/self-regex.py
@@ -4,8 +4,12 @@ import sys
 import urllib2
 from base64 import b64decode
 
-bl_file = 'gfw.url_regex.lst'
-wh_file = 'cn.url_regex.lst'
+bl_file = 'gfw_dstdom_regex'
+acl_name_bl = 'gfw'
+wh_file = 'cn_dstdom_regex'
+acl_name_wh = 'cn'
+
+acl_type = 'dstdom_regex'
 
 def parse_line(line):
 	if line[0] == '/' and line[-1] == '/':   #regex line
@@ -15,13 +19,15 @@ def parse_line(line):
 	line = line.replace('(',r'\(').replace(')',r'\)')
 
 	if line.startswith('||'):
-		return '^https?:\/\/%s.*' % line[2:]
+		return '.' + line[2:]
+	elif line.startswith('|https://'):
+		return line[9:]
+	elif line.startswith('|http://'):
+		return line[8:]
 	elif line.startswith('|'):
-		return '^%s.*' %line[1:]
-	elif line[-1] == '|':
-		return '.*%s$' % line
+		return line[1:]
 	else:
-		return '.*%s.*' % line
+		return line
 
 def convert(gfwlist):
 	black = open(bl_file,'w')
@@ -31,9 +37,9 @@ def convert(gfwlist):
 			continue
 
 		if l.startswith('@@'):
-			white.write(parse_line(l[2:]) + '\n')
+			white.write('acl ' + acl_name_wh + ' ' + acl_type + ' ' + parse_line(l[2:]) + '\n')
 		else:
-			black.write(parse_line(l) + '\n')
+			black.write('acl ' + acl_name_bl + ' ' + acl_type + ' ' + parse_line(l) + '\n')
 
 def main():
 	print "main test"
@@ -46,7 +52,7 @@ def main():
 
 	src = open(sys.argv[1]).read()
 	src = b64decode(src)
-	print src
+	# print src
 	convert(src)
 
 
