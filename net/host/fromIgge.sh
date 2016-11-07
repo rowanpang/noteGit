2497b8aa0ea2cd1d176cb8e4678a1e03e9bf7935
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-11-07 20:09:47 +0800

    1,google hosts

--- net/host/fromIgge.sh
+++ net/host/fromIgge.sh
@@ -2,7 +2,36 @@
 
 target=hosts-$(date +%Y%m%d)
 #echo $target
-wget $1 | tee $target
+date +%M%S
+#wget --no-check-certificate https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O $target
 ln -sf $target hosts
 
+git add $target
+git add hosts
 
+function isExist() {
+	echo "test $1"
+	[ $1 == "hosts" ] || [ $1 == $target ] || return 1
+}
+
+function pureCommit {
+	for changed in `git status -s | awk {'print $2'}`;do
+		isExist $changed
+		if [  $? -eq 0 ];then
+			echo "exist"
+		else
+			echo "not exist"
+			return 1
+		fi
+	done
+}
+
+pureCommit
+
+if [ $? -eq 0 ];then
+	echo "can commit"
+	git commit
+else
+	echo "can not auto commit,manual do it"
+fi
+		
