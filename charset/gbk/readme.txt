cc3e05f929612ef139c8308da0066f9f6e0b8e89
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-11-09 20:33:24 +0800

    1,编码相关测试优化， 2，编码及fat文件系统的关系，nls测试.

--- charset/gbk/readme.txt
+++ charset/gbk/readme.txt
@@ -5,3 +5,7 @@
 3, 庞
 	gbk: 0xc5d3
 	"p\305\323\316\260\325\361"	
+
+4,with fat
+	a,因为文件名时gbk编码，所以mounts时需要制定对应的iocharset=cp936,这样才不会出错.
+	b,而在linux下如果想要正常列出文件名，iocharset 应该为 utf8,这是一个冲突项.
--- charset/utf8/main_utf8.c
+++ charset/utf8/main_utf8.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
+	int ret;
 	printf("pangwz---test\n");
 	printf("pangwz-庞伟振\n");
-
-	printf("pangwz█\n");
-	printf("pangwz%c%c%c\n",0xe2,0x96,0x88);
-	printf("pangwz%c%c%c\n",0xe2,0x93,0xb5);
-
-	char in[4]={"庞"};
-
-	printf("%#x ",in[0]);
-	printf("%#x ",in[1]);
-	printf("%#x \n",in[2]);
 	
-	char a = 'p';
-	printf("%#x \n",a);
-	return 0;
+	char *name = "/media/ROWAN/p庞伟振";
+	FILE *f1 = fopen(name,"w+");
+	char *content = "utf8 src coding program,write to new file,p庞伟振";
+	ret = fwrite(content,1,strlen(content),f1);
+	printf("result %d, %d\n",__LINE__,ret);
+
+	fclose(f1);	
+	/*ret = unlink("庞伟振");*/
+
+	printf("result %d\n",ret);
+	return ret;
 }
+ 
