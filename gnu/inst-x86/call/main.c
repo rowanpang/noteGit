ec74ef73fd339a31b5de1c4c4ce124ef786f6360
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-11-23 18:17:19 +0800

    1,x86-call inst test suit

---
+++ gnu/inst-x86/call/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void show(char* msg)
+{
+	printf("%s\n",msg);
+}
+
+
+char *msg="pagnwz";
+int main(int argc,char **argv)
+{
+	show("msg");
+	printf("hello world\n");
+	return 0;
+}
