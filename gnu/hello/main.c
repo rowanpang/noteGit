48c08e93d49def2357632d3ced14409a3c99f16f
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-11-29 22:38:13 +0800

    1,more test

--- gnu/hello/main.c
+++ gnu/hello/main.c
@@ -9,6 +9,10 @@ void show(char* msg)
 char *msg="pagnwz";
 int main(int argc,char **argv)
 {
+	int a = 9;
+	int b = -1;
+	int c = a - b;
+	int d = 3- (-1);
 	printf("hello world\n");
 	show("msg");
 	return 0;
