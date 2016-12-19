86a6ced4a0f51ca19c54de2e32b3984d94ab14b5
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-12-19 14:23:42 +0800

    add setxkbmod dep for keyboar init

--- osInit.sh
+++ osInit.sh
@@ -65,12 +65,13 @@ function initCheck(){
 xorgKeyboardConf=/etc/X11/xorg.conf.d/00-keyboard.conf
 function initKeyBoard(){
 	#swapcaps,ctrl
+	pkgCheckInstall xorg-x11-xkb-utils
 	setxkbmap -option ctrl:swapcaps
 	if [ -f $xorgKeyboardConf ];then
 		local count=`grep -c 'ctrl:swapcaps' $xorgKeyboardConf`
 		if [ $count -eq 0 ];then 
 			verbose "modify $xorgKeyboardConf"
-			sed -i '/EndSection/ i \\tOption "XKbOptions" "ctrl:swapcaps"' $xorgKeyboardConf
+			sudo sed -i '/EndSection/ i \\tOption "XKbOptions" "ctrl:swapcaps"' $xorgKeyboardConf
 		else
 			verbose "$xorgKeyboardConf had configured"
 		fi
