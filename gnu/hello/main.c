00a62dca9fd13204ee496c9bf303a98092c56cf2
Rowan Pang <pangweizhen.2008@hotmail.com>
2016-12-22 19:31:21 +0800

    add sleep option

--- gnu/hello/main.c
+++ gnu/hello/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void show(char* msg)
 {
 	printf("%s\n",msg);
 }
-
 
 char *msg="pagnwz";
 int main(int argc,char **argv)
@@ -13,7 +14,13 @@ int main(int argc,char **argv)
 	int b = -1;
 	int c = a - b;
 	int d = 3- (-1);
+	int sec=0;
 	printf("hello world\n");
 	show("msg");
+	if (argc > 1){
+		sec = atoi(argv[1]);
+		printf("sleep %ds\n",sec);
+		sleep(sec);
+	}
 	return 0;
 }
