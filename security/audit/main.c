9b455ffced013e15b7ba6cc795ad46abf1465409
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-02-22 20:24:51 +0800

    1,add Makefile for test

--- security/audit/main.c
+++ security/audit/main.c
@@ -1,5 +1,5 @@
-
 #include<stdio.h>
+#include<unistd.h>
 #include<libaudit.h>
 #include<string.h>
 #include<errno.h>
@@ -11,22 +11,18 @@ int main(int argc,char *argv[])
 	
 	int auditfd=0;
 	auditfd = audit_open();
-	if(auditfd == -1)
-	{
+	if(auditfd == -1) {
 		printf("audit_open error,case:%s\n",strerror(errno));
 	}
 	
 	int ret = audit_log_user_message(auditfd,AUDIT_USER_LOGIN,"PANGWZ-AUDIT test",NULL,NULL,NULL,1);
-	if(ret <=0)
-	{
+	if(ret <=0) {
 		printf("audit_log error,case:%s\n",strerror(errno));
-	}
-	else
-	{
+	} else {
 		printf("ret value e.g message sequence: %d\n",ret);
 	}	
+
 	close(auditfd);
-	
 
 	return 0;
 }
