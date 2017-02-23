1e32700277c0ba2c991d79404269cd07849ce7e2
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-02-23 19:14:53 +0800

    audit api ret value with error

---
+++ net/info/hostinfo.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+int main(int argc, char **argv)
+{
+    if(argc != 2){
+         fprintf(stderr, "usage: %s <domain name or dotted-decimal>\n", argv[0]);
+         return 0;
+    }
+
+   char **pp;
+   struct in_addr addr;
+   struct hostent *hostp;
+
+   if(inet_aton(argv[1], &addr) != 0){
+       hostp = gethostbyaddr((const char *)&addr, sizeof(addr), AF_INET);
+   }else{
+       hostp = gethostbyname(argv[1]);
+   }
+
+   printf("official hostname: %s\n", hostp->h_name);
+
+   for(pp = hostp->h_aliases; *pp != NULL; pp++){
+        printf("alias: %s\n", *pp);
+   }
+
+   for(pp = hostp->h_addr_list; *pp != NULL; pp++){
+        addr.s_addr = ((struct in_addr *) *pp)->s_addr;
+        printf("address: %s\n", inet_ntoa(addr));
+   }
+
+   return 0;
+}
--- security/audit/main.c
+++ security/audit/main.c
@@ -6,19 +6,25 @@
 
 int main(int argc,char *argv[])
 {
-    printf("test for audit log...\n");
-    
     int auditfd=0;
+    int ret;
     auditfd = audit_open();
     if(auditfd == -1) {
-	    printf("audit_open error,case:%s\n",strerror(errno));
+	printf("audit_open error,case:%s\n",strerror(errno));
     }
     
-    int ret = audit_log_user_message(auditfd,AUDIT_USER_LOGIN,"PANGWZ-AUDIT test",NULL,NULL,NULL,1);
+    ret = audit_log_user_message(auditfd,AUDIT_USER_LOGIN,"PANGWZ-AUDIT test",NULL,NULL,NULL,1);
     if(ret <=0) {
-	    printf("audit_log error,case:%s\n",strerror(errno));
+	printf("error,case:%s ret:%d\n",strerror(errno),ret);
     } else {
-	    printf("ret value e.g message sequence: %d\n",ret);
+	printf("ret value e.g message sequence: %d\n",ret);
+    }	
+
+    ret = audit_log_user_avc_message(auditfd,AUDIT_USER_LOGIN,"byAUDIT test",NULL,NULL,NULL,1);
+    if(ret <=0) {
+	printf("1-error,case:%s ret:%d\n",strerror(errno),ret);
+    } else {
+	printf("1-ret value e.g message sequence: %d\n",ret);
     }	
 
     close(auditfd);
