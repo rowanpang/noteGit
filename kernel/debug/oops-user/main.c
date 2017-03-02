cfc3998a8564572916b61ddf9e8a78db9e1ad2a2
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-02 21:52:49 +0800

    user space segment fault

--- kernel/debug/oops-user/main.c
+++ kernel/debug/oops-user/main.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include<unistd.h>
 
 int main(int argc,char** argv)
 {
     int a = 100;
     int b = 0;
+    int c;
     char *pb=0;
+    
+    printf("cur pid:%d\n",getpid());
+
     *pb = 1;
-    int c = a/b;
+    c = a/b;
 
     return 0;
 }
