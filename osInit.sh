c0c92ffc252f554b384d3baba94f56df2a4ea4e7
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-03 17:13:47 +0800

    1,enable xfce4-terminal

--- osInit.sh
+++ osInit.sh
@@ -148,61 +148,69 @@ function initI3wm(){
         #3. ~/.i3/config
         #4. /etc/i3/config
 
-    local dir=${TOOLSDIR}i3wm/
-    pkgCheckInstall i3
-    pkgCheckInstall i3lock
-    #pkgCheckInstall i3-doc      #about 124Mib,too big ignore
-    if [ ! -d $dir ];then
-        git clone git@github.com:rowanpang/i3.git $dir
-        ln -rsf ${dir}config-v4.12 ${dir}config    
-        ln -s $dir ${HOMEDIR}.i3
-    else
-        verbose "$dir exist" 
-    fi
+    #base 
+	local dir=${TOOLSDIR}i3wm/
+	pkgCheckInstall i3
+	pkgCheckInstall i3lock
+	#pkgCheckInstall i3-doc      #about 124Mib,too big ignore
+	if [ ! -d $dir ];then
+	    git clone git@github.com:rowanpang/i3.git $dir
+	    ln -rsf ${dir}config-v4.12 ${dir}config    
+	    ln -s $dir ${HOMEDIR}.i3
+	else
+	    verbose "$dir exist" 
+	fi
 
     #polkit-gnome
-    pkgCheckInstall polkit-gnome
+	pkgCheckInstall polkit-gnome
     #wallpaper
-    pkgCheckInstall feh 
+	pkgCheckInstall feh 
     #xtrlock
-    pkgCheckInstall pam-devel
+	pkgCheckInstall pam-devel
     #memSave
-    pkgCheckInstall usermode-gtk
-    pkgCheckInstall i3lock
-    local memSaveDir="${dir}screenlock/memSave/"
-    local pamConf="/etc/pam.d/system-auth"
-    sed -i "s;PROGRAM=.*;PROGRAM=${memSaveDir}memSave.sh;" ${memSaveDir}memSave.consolehelper
-    lsudo sed -i 's;auth\s\+sufficient\s\+pam_unix.so.*;& nodelay;' $pamConf                #disable pwd error delay
-    lsudo ln -sf ${memSaveDir}memSave.consolehelper /etc/security/console.apps/memSave
-    lsudo ln -sf ${memSaveDir}memsave.pam /etc/pam.d/memsave
-    lsudo ln -rsf `which consolehelper` /usr/bin/memSave 
+	pkgCheckInstall usermode-gtk
+	pkgCheckInstall i3lock
+	local memSaveDir="${dir}screenlock/memSave/"
+	local pamConf="/etc/pam.d/system-auth"
+	sed -i "s;PROGRAM=.*;PROGRAM=${memSaveDir}memSave.sh;" ${memSaveDir}memSave.consolehelper
+	lsudo sed -i 's;auth\s\+sufficient\s\+pam_unix.so.*;& nodelay;' $pamConf                #disable pwd error delay
+	lsudo ln -sf ${memSaveDir}memSave.consolehelper /etc/security/console.apps/memSave
+	lsudo ln -sf ${memSaveDir}memsave.pam /etc/pam.d/memsave
+	lsudo ln -rsf `which consolehelper` /usr/bin/memSave 
 
     #---------disable pwd quality check
-    local oldUnix="$(sed --quiet '/^password\s\+\S\+\s\+pam_unix.so.*/ p' $pamConf)"
-    local newUnix=$(echo "$oldUnix" | sed 's/use_authtok//')
-    lsudo sed -i 's/^password\s\+\S\+\s\+pam_pwquality.so.*/#&/' $pamConf
-    lsudo sed -i 's/^password\s\+\S\+\s\+pam_cracklib.*/#&/' $pamConf
-    if [ "$oldUnix" != "$newUnix" ];then
-        lsudo sed -i 's/^password\s\+\S\+\s\+pam_unix.so.*/#&/' $pamConf
-        lsudo sed -i "/#password\s\+\S\+\s\+pam_unix.so.*/a$newUnix" $pamConf
-    fi
+	local oldUnix="$(sed --quiet '/^password\s\+\S\+\s\+pam_unix.so.*/ p' $pamConf)"
+	local newUnix=$(echo "$oldUnix" | sed 's/use_authtok//')
+	lsudo sed -i 's/^password\s\+\S\+\s\+pam_pwquality.so.*/#&/' $pamConf
+	lsudo sed -i 's/^password\s\+\S\+\s\+pam_cracklib.*/#&/' $pamConf
+	if [ "$oldUnix" != "$newUnix" ];then
+	    lsudo sed -i 's/^password\s\+\S\+\s\+pam_unix.so.*/#&/' $pamConf
+	    lsudo sed -i "/#password\s\+\S\+\s\+pam_unix.so.*/a$newUnix" $pamConf
+	fi
     #end---------disable pwd quality check
+
     #terminal for nautilus
-    pkgCheckInstall gnome-terminal-nautilus
+	pkgCheckInstall gnome-terminal-nautilus
 
     #volume
-    pkgCheckInstall volumeicon
-    pkgCheckInstall pavucontrol
-    #sed -i "/^#volumeicon/ aexec /usr/bin/volumeicon" ${dir}config
+	pkgCheckInstall volumeicon
+	pkgCheckInstall pavucontrol
+	#sed -i "/^#volumeicon/ aexec /usr/bin/volumeicon" ${dir}config
 
     #nm-applet
-    pkgCheckInstall network-manager-applet
-    #sed -i "/^#nm-applet/ aexec /usr/bin/nm-applet" ${dir}config
+	pkgCheckInstall network-manager-applet
+	#sed -i "/^#nm-applet/ aexec /usr/bin/nm-applet" ${dir}config
 
     #terminator
-    pkgCheckInstall terminator
-    mkdir -p ${HOMEDIR}.config/terminator
-    ln -sf ${dir}dep/terminator/config ${HOMEDIR}.config/terminator/config 
+	pkgCheckInstall terminator
+	mkdir -p ${HOMEDIR}.config/terminator
+	ln -sf ${dir}dep/terminator/config ${HOMEDIR}.config/terminator/config 
+
+    #xfce4-terminator
+	local confDir=${HOMEDIR}.config/xfce4/terminal
+	pkgCheckInstall xfce4-terminal-0.6.3
+	mkdir -p $confDir
+	ln -sf ${dir}dep/xfce4-terminal/* $confDir
 }
 
 function initNutstore(){
