dbace0a681ee4dceef2084cac9e236a8cd04b3f0
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-09 10:48:23 +0800

    1,replease dot '.' 2,replease '\n' use sed

--- shell/string.sh
+++ shell/string.sh
@@ -12,3 +12,15 @@ echo $spaceString
 
 str="abcd"
 echo ${str:0:10}
+
+fileName="module.c.conf"
+echo ${fileName/./_}
+echo ${fileName//./_}
+
+testOut="asdfasdf   
+adfkajdfla"
+
+testOut2="$(echo "$testOut" | sed ':a;N;$!ba;s#\n#\\n#')"
+echo "$testOut"
+echo -e $testOut
+echo $testOut2
