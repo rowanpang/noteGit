2df7097a11a378806d706c576ce3dfe2a7eb6864
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-10 18:38:43 +0800

    1,true && echo "true" || echo "false" expression

--- shell/groupCmd.sh
+++ shell/groupCmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#ref https://unix.stackexchange.com/questions/88850/precedence-of-the-shell-logical-operators
+
+false && echo "true" || echo "false"	    #echo 'false‘
+true && echo "true" || echo "false"	    #echo 'true'
 
 echo "test1" || { echo "sadfddd" && exit -1; }
-
 echo "finish"
