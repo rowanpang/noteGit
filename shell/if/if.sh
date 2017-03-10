4a1563adab1bf0b8f667b7e1215b7f10025b75af
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-10 19:47:49 +0800

    1,add usage of $?

--- shell/if/if.sh
+++ shell/if/if.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 if ! grep -q "self" ./test.txt;then
-	echo "grep ok";
+    echo "grep ok";
 else
-	echo "grep ng"
+    echo "grep ng"
+fi
+
+if [ 0 ];then
+    echo "0 is true"
+else
+    echo "0 is false"
+fi
+
+if [ 1 ];then
+    echo "1 is true"
+else
+    echo "1 is false"
+fi
+
+if [ $? -eq 0 ];then
+    echo "0 is true"
+else
+    echo "0 is false"
 fi
