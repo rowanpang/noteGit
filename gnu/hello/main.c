8e793b35dd2998efb00ed0954917ec64b95010d4
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-21 19:49:31 +0800

    1,tapstop optimize

--- gnu/hello/main.c
+++ gnu/hello/main.c
@@ -4,23 +4,23 @@
 
 void show(char* msg)
 {
-	printf("%s\n",msg);
+    printf("%s\n",msg);
 }
 
 char *msg="pagnwz";
 int main(int argc,char **argv)
 {
-	int a = 9;
-	int b = -1;
-	int c = a - b;
-	int d = 3- (-1);
-	int sec=0;
-	printf("hello world\n");
-	show("msg");
-	if (argc > 1){
-		sec = atoi(argv[1]);
-		printf("sleep %ds\n",sec);
-		sleep(sec);
-	}
-	return 0;
+    int a = 9;
+    int b = -1;
+    int c = a - b;
+    int d = 3- (-1);
+    int sec=0;
+    printf("hello world\n");
+    show("msg");
+    if (argc > 1){
+        sec = atoi(argv[1]);
+        printf("sleep %ds\n",sec);
+        sleep(sec);
+    }
+    return 0;
 }
