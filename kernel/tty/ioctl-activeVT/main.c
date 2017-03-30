6b6df3698a7e0599b3752784f95848243d9fcb4d
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-31 00:12:58 +0800

    1,change to vt even under gnome.

---
+++ kernel/tty/ioctl-activeVT/main.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<linux/kd.h>
+#include<linux/vt.h>
+
+typedef _Bool bool;
+
+int main(int argc,char **argv)
+{
+    int ret=0;
+    int fd;
+    int ttyX=1;
+#define DEVPREFIX "/dev/tty"
+    char devpath[20];
+    int vtTarget=3;
+
+    if (argc >1){
+	vtTarget = atoi(argv[1]);
+    }
+    printf("target vt is %d\n",vtTarget);
+    sprintf(devpath,"%s%s",DEVPREFIX,"0"); //printf("target dev is %s\n",devpath);
+
+    ret = open(devpath,O_RDONLY);
+    if (ret == -1) {
+	perror("open target dev error");
+	ret = errno;
+	goto OUT;
+    }
+    fd = ret; 
+    
+    ret = ioctl(fd,VT_ACTIVATE,vtTarget);
+    if (ret == -1) {
+	perror("error for VT_ACTIVATE");
+	ret = errno;
+	goto CLOSE;
+    }else{
+	printf("switch to vt:%d\n",vtTarget);
+    }
+
+CLOSE: 
+    close(fd);
+OUT:
+    return ret;
+}
