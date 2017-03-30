960c8368a6807883bffb69d0770d910bcb119a87
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-03-30 19:31:11 +0800

    1,add ioctl example, got tty work mode.

---
+++ fs/ioctl/main.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<linux/kd.h>
+
+typedef _Bool bool;
+
+int main(int argc,char **argv)
+{
+    int ret=0;
+    int fd;
+    int ttyX=1;
+#define DEVPREFIX "/dev/tty"
+    char devpath[20];
+    int ledState;
+    bool ttyMode;	//force to match str array.
+    char *ttyModeStr[2]={
+	"text",
+	"graphics",
+    };
+
+    if (argc >1){
+	sprintf(devpath,"%s%s",DEVPREFIX,argv[1]);
+    }else{
+	sprintf(devpath,"%s%s",DEVPREFIX,"1");
+    }
+    
+    printf("target dev is %s\n",devpath);
+
+    ret = open(devpath,O_RDONLY);
+    if (ret == -1) {
+	perror("open target dev error");
+	ret = errno;
+	goto OUT;
+    }
+    fd = ret; 
+
+    ret = ioctl(fd,KDGETLED,&ledState);
+    if (ret == -1) {
+	perror("error for KDGETLED");
+	ret = errno;
+	goto CLOSE;
+    }
+    printf("led state :%d\n",ledState);
+
+    ret = ioctl(fd,KDGETMODE,&ttyMode);
+    if (ret == -1) {
+	perror("error for KDGETLED");
+	ret = errno;
+	goto CLOSE;
+    }
+    printf("tty work mode state :%s\n",ttyModeStr[ttyMode]);
+
+CLOSE: 
+    close(fd);
+OUT:
+    return ret;
+}
