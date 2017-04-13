f705e86f4abaa4683c2257fe6ec3e0e11394c085
pangwz <pangwz@kvm-c7.lan>
2017-04-13 13:00:14 +0800

    1,add ftace kapi info

--- kernel/debug/ftrace/ftrace-deamon.c
+++ kernel/debug/ftrace/ftrace-deamon.c
@@ -2,12 +2,14 @@
 
 static int ftrace_daemon_init(void)
 {
+    trace_printk("ftrace daemon \n");
     pr_info("function:%s\n",__FUNCTION__);
     return 0;
 }
 
 static void ftrace_daemon_exit(void)
 {
+    trace_printk("ftrace daemon unloading\n");
     pr_info("function:%s\n",__FUNCTION__);
     return ;
 }
