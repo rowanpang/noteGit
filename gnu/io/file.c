841b22aa9617fa13f36a91cc42f89a1b472b3ad9
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-05-12 21:14:43 +0800

    add simple suit

--- gnu/io/file.c
+++ gnu/io/file.c
@@ -13,16 +13,23 @@ int main(int argc, char** argv) {
     int fd;
     int i;
 
-    fd = open(argv[1],O_RDONLY);
+    fd = open(argv[1],O_RDWR);
+    dprintf(fd,"fd is :%d\n",fd);
+    dprintf(STDOUT_FILENO,"fd is :%d\n",fd);
 
     write(STDOUT_FILENO, "Hello, World\n", 13);
+    write(fd, "Hello, World\n", 13);
     ret = read(fd,buf,sizeof(buf));
     printf("ret:%d,readin %d\n",ret,strlen(buf));
 
     for (i=0;i<ret;i++){
 	printf("%#x ",buf[i]);
+	dprintf(fd,"%#x ",buf[i]);
     }
     printf("\n------------\n");
+    dprintf(fd,"\n------------\n");
+
+    sleep(120);
 
     close(fd);
 }
