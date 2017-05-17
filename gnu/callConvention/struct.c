6a6bf63e18fe66339c0c2cb4acc839d0996e2577
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-05-17 16:22:04 +0800

    1,add long ,float return type test

--- gnu/callConvention/struct.c
+++ gnu/callConvention/struct.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+float floatRet(){
+    return 2.2;
+}
+
+long longRet(){
+    return 2;
+}
+
 int hello(int ok,int arg2,int arg3){
     int doit;
     int a,b;
@@ -13,7 +21,12 @@ int hello(int ok,int arg2,int arg3){
 int main(int argc,char** argv)
 {
     int ret;
+    long retLong;
+    float retfloat;
     ret = hello(1,2,3);
+    retLong = longRet();
+    retfloat = floatRet();
+
     return ret;
 }
 
