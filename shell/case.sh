154f3c4ad194990dd2c071686666250645b11b4c
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-05-18 11:24:46 +0800

    1,git add more info for case expression

--- shell/case.sh
+++ shell/case.sh
@@ -1,19 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+while [ "aa" ];do
+    echo -n "please input:"
+    read aNum
+    case $aNum in
+	    1)  
+		echo 'You select 1'
+		;;
+	    [2-4])
+		echo "match [2-4],$aNum"
+		;;
+	    5|6)  
+		echo 'You select 5 or 6'
+		;;
+	    [:digit:])				#ng
+		echo "match [:digit:],$aNum"
+		;;
+	    [8-12])				#ng
+		echo "match [8-12],$aNum"
+		;;
+	    [7-9][7-9])				
+		echo "match [7-9][7-9],$aNum"
+		;;
+	    ?[7-9])				#ng
+		echo "match ?[7-9],$aNum"
+		;;
+	    [abcd])				#match single a,b,c or d
+		echo "match [abcd],$aNum"
+		;;
+	    [abcd]*)				#match string start with a,b,c or d
+		echo "match [abcd]*,$aNum"
+		;;
 
-echo 'Input a number between 1 to 6'
-read aNum
-echo 'Your number is:\c'
-case $aNum in
-	1)  echo 'You select 1'
-	    ;;
-	2)  echo 'You select 2'
-	    ;;
-	3)  echo 'You select 3'
-	    ;;
-	4)  echo 'You select 4'
-	    ;;
-	5|6)  echo 'You select 5 or 6'
-	    ;;
-	*)  echo 'You do not select a number between 1 to 4'
-	;;
-esac
+	    *)  
+		echo 'no match'
+		;;
+    esac
+done
