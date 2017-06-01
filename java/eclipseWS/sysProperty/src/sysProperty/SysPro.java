b5833d64d4b771f55d94b542a0c171b8de3acf55
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-01 20:33:21 +0800

    1,more method for query System.propertys. 2,java lambed (consumer) example.

--- java/eclipseWS/sysProperty/src/sysProperty/SysPro.java
+++ java/eclipseWS/sysProperty/src/sysProperty/SysPro.java
@@ -1,9 +1,51 @@
 package sysProperty;
 
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.function.Consumer;
+
 public class SysPro {
 
     public static void main(String[] args) {
+	String proName = null;
+	TreeSet<String> sortedNames = new TreeSet<>();
+	
+	@SuppressWarnings("unchecked")
+	Enumeration<String> proNames = (Enumeration<String>) System.getProperties().propertyNames();
+
+	//use treeset to sort keys;
+	while (proNames.hasMoreElements()){
+	    proName = proNames.nextElement();
+	    sortedNames.add(proName);
+	}
+
+	//query keys --method.1
+/*	for (String sortName: sortedNames){
+            System.out.println(sortName + "=" + System.getProperty(sortName));
+	}
+*/	
+	//query keys --method.2
+/*	Iterator<String> treeIt = sortedNames.iterator();
+	while (treeIt.hasNext()){
+	    proName = treeIt.next();
+            System.out.println(proName + "=" + System.getProperty(proName));
+	}
+*/	
+	Consumer<String> itAction = SysPro::msgOut;
+	sortedNames.forEach(itAction);
+
+//	Consumer<String> itAction2 = (pro) -> System.out.println(pro + "=" + System.getProperty(pro));
+//	sortedNames.forEach(itAction2);
+	
+/*	System.out.println(System.getProperties().propertyNames().toString());
+	System.getProperties().list(System.out);
 	System.out.println(System.getProperties());
+*/    
+    }
+    
+    private static void msgOut(String pro){
+	System.out.println(pro + "=" + System.getProperty(pro));
     }
 
 }
