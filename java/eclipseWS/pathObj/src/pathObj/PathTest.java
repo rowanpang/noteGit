b3f794a6d2048b0e93e4b7bd4cf47ad3bf0fb5c3
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-02 16:07:11 +0800

    java File object suit

--- java/eclipseWS/pathObj/src/pathObj/PathTest.java
+++ java/eclipseWS/pathObj/src/pathObj/PathTest.java
@@ -1,5 +1,9 @@
 package pathObj;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class PathTest {
 
     public static void main(String[] args) {
@@ -7,9 +11,28 @@ public class PathTest {
 	System.out.println(System.getProperty("user.home"));
 	System.out.println(System.getProperty("java.home"));
 	System.out.println(System.getProperty("java.io.tmpdir"));
-	System.out.println(System.getProperty("file.separator"));
 	System.out.println(System.getProperty("java.class.path"));
+	System.out.println(System.getProperty("file.separator"));
+	
+	PathTest pt = new PathTest();
+        pt.getCurPath();
 
+    }
+    
+    private String getCurPath() {
+	
+	File dir = new File("a.txt");
+        try {
+	    System.out.println(dir.getCanonicalPath());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+        System.out.println(dir.getAbsolutePath());
+
+        System.out.println(dir.getPath());
+
+	return "ok";
     }
 
 }
