3ad0a1df1ddf4f5b65f19f8407924e3055556dc2
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-02 14:14:18 +0800

    1,add filepath case

---
+++ java/eclipseWS/pathObj/src/pathObj/PathTest.java
@@ -0,0 +1,15 @@
+package pathObj;
+
+public class PathTest {
+
+    public static void main(String[] args) {
+	System.out.println(System.getProperty("user.dir"));
+	System.out.println(System.getProperty("user.home"));
+	System.out.println(System.getProperty("java.home"));
+	System.out.println(System.getProperty("java.io.tmpdir"));
+	System.out.println(System.getProperty("file.separator"));
+	System.out.println(System.getProperty("java.class.path"));
+
+    }
+
+}
