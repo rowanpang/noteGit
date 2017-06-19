f696b8caea6dc0cc69a3084af4f153e9959895ba
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-19 14:44:41 +0800

    preload test suit

---
+++ gnu/preload/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc,char** argv)
+{
+    char* pwd = "askdf";
+    /*if (!strcmp("asdf","abcd")){ this format will preprocess,so always in-correct*/ 
+    if (!strcmp(pwd,"abcd")){
+	printf("correct\n");
+    }else{
+	printf("in-correct\n");
+    }
+    printf("hello world,pid:%d\n",getpid());
+
+    sleep(300);
+    
+    return 0;
+}
---
+++ gnu/preload/msg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/*int printf (const char *__restrict __format, ...)*/
+/*{*/
+    /*puts("by prload so\n");*/
+    /*puts(__format);*/
+    /*return 0;*/
+/*}*/
+
+
+int strcmp(const char* s1,const char* s2)
+{
+    printf("preload invoke\n");
+    return 0; 
+}
+
