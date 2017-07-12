a2c5f3c9626c0f6c326d07447163ef68dd45d555
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-07-13 00:40:19 +0800

    python cmdline args parser

---
+++ python/cmdArgs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys, getopt
+
+opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
+
+input_file=""
+output_file=""
+
+for op, value in opts:
+    if op == "-i":
+        input_file = value
+    elif op == "-o":
+        output_file = value
+    elif op == "-h":
+        usage()
+	sys.exit()
