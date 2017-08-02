4ac3cfa794b1d783906b3e49e25079e75b155bf2
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-08-02 18:50:49 +0800

    add api call example

--- fs/disk-mbr-ebr/sdb/vbr-fat.py
+++ fs/disk-mbr-ebr/sdb/vbr-fat.py
@@ -7,10 +7,17 @@ import fatMeta
 
 diskblock='/dev/sdb1'
 diskblock='/dev/sdc1'
+# diskblock='/dev/sdc2'
 # diskblock='./vbr-fat16.bin'
 fvbr = fatMeta.fatVBR(diskblock)
 
 fvbr.parser_fields()
 
 # hexdump.strHexdump(fvbr.get_FAT())
-hexdump.strHexdump(fvbr.get_rootdir_content())
+# b= fvbr.read_cluster(2)
+# b,i = fvbr.read_cluster_chain(2,0)
+# hexdump.strHexdump(b)
+
+# hexdump.strHexdump(fvbr.get_rootdir_content())
+fvbr.list_root()
+
