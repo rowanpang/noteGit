b87675115d9956216feb617c1ab4c373f4717f2e
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-08-18 09:08:01 +0800

    len('庞') for diff encodings str.

--- python/encoding.py
+++ python/encoding.py
@@ -3,10 +3,10 @@
 
 import binascii
 
-strFileCoding= '庞伟振'
-print('type of strFileCoding: %s' %(type(strFileCoding)))
-strFileCoding_decode = strFileCoding.decode('utf8')
-print('type of strFileCoding_decode: %s' %(type(strFileCoding_decode)))
+strU8= '庞伟振'
+print('type of strU8: %s' %(type(strU8)))
+strU8_decode = strU8.decode('utf8')
+print('type of strU8_decode: %s' %(type(strU8_decode)))
 
 strUnicode = u'庞伟振'
 print('type of strUnicode: %s' %(type(strUnicode)))
@@ -17,3 +17,14 @@ print('hexlify(strUnicode):%s' %(binascii.hexlify(strUnicode.encode('utf8'))))
 
 strUnicode_encode = strUnicode.encode('utf32')
 print('type of strUnicode_encode: %s' %(type(strUnicode_encode)))
+
+print len(u'a')
+print len(u'庞')
+print len('庞')
+strU8='庞'
+print '庞-gb18030',len(strU8.decode('utf8').encode('gb18030'))
+print binascii.hexlify(strU8.decode('utf8').encode('gb18030'))
+
+strU8='a'
+print 'a-gb18030',len(strU8.decode('utf8').encode('gb18030'))
+print binascii.hexlify(strU8.decode('utf8').encode('gb18030'))
