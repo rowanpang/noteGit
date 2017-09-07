502a03e5a8a7ad558013aebf833db5e5923a2417
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-09-07 14:09:04 +0800

    [python]     update comment 'set -> setted'

--- python/maxbits.py
+++ python/maxbits.py
@@ -4,7 +4,7 @@
 i = 1<< 17 | 1 << 16 | (1 << 4) + 145
 print 'test number: %x' %i
 j = 0       # max bit num
-k = 0       # number of bits set
+k = 0       # number of bits setted
 while (i > 0):
     j += 1
     if (i & 1):
