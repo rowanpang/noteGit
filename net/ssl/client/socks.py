e8810efdd5228d5cdae0f1facc41e1aa52acd41b
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-10-10 21:00:34 +0800

    [net ssocks]     add more suit info

--- net/ssl/client/socks.py
+++ net/ssl/client/socks.py
@@ -4,7 +4,15 @@ import ssl,socket
 import pprint
 from ssl import Purpose
 
+print ssl.get_default_verify_paths()
+
+print ssl.get_server_certificate(("www.baidu.com",443))
+
 ctx = ssl.create_default_context(Purpose.SERVER_AUTH)
+certs = ctx.get_ca_certs()
+for cert in certs:
+    pprint.pprint(cert['subject'])
+
 ctx.check_hostname = False
 
 scon = ctx.wrap_socket(socket.socket(socket.AF_INET),server_hostname = "sss")
@@ -13,12 +21,10 @@ cert = scon.getpeercert()
 # pprint.pprint(cert)
 scon.sendall(b"HEAD / HTTP/1.1\r\n Host:baidu.com\r\n\r\n")
 pprint.pprint(scon.recv(1024).split(b"\r\n"))
+
+
+
+
+
 scon.close()
 
-scon = ctx.wrap_socket(socket.socket(socket.AF_INET),server_hostname = "sss")
-scon.connect(("www.360.com",443))
-cert = scon.getpeercert()
-# pprint.pprint(cert)
-scon.sendall(b"GET / HTTP/1.0\r\n Host: www.360.com\r\n\r\n")
-pprint.pprint(scon.recv(1024).split(b"\r\n"))
-scon.close()
