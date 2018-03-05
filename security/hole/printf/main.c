668004f81e1c5c8a52a1b3f38220b7c170241773
Rowan Pang <pangweizhen.2008@hotmail.com>
2018-03-05 14:24:03 +0800

    [security hole]     adjust hole dir

---
+++ security/hole/printf/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+    TODO: https://www.jianshu.com/p/f2acfeb66b6c
+
+
+    return 0;
+}
