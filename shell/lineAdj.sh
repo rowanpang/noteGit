b8752f5fd1f67c1eb5bacd64bb96694a62444351
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-01-29 14:14:44 +0800

    line adj

--- shell/lineAdj.sh
+++ shell/lineAdj.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 function lineadj(){
     ln=$1
+    lo=$2
     bs=""
     nsp=""
-    while [ $ln -ge 1 ];do
+
+    lb=$ln
+    if [ $ln -lt $lo ];then
+	((ld=lo-ln))
+	while [ $ld -ge 1 ];do
+	    nsp="$nsp "
+	    ((ld=ld-1))
+	done
+	echo -en "$nsp"
+
+	lb=$lo
+    fi
+
+    while [ $lb -ge 1 ];do
         bs="$bs\\b"
-	nsp="$nsp "
-        ((ln=ln-1))
+        ((lb=lb-1))
     done
-    echo -en "$bs\b\b"
-    echo -en "$nsp  "
     echo -en "$bs\b\b"
 }
 
 sek=-5
 echo "60 Seconds Wait!"
+echo -en "\e[?25l"
 echo -n "One Moment please "
 lo=0
 while [ $sek -lt 15 ]
@@ -22,12 +34,10 @@ do
     bs=""
     echo -n "$sek s"
     ln=${#sek}
-    sleep 1
     sek=$[$sek+1]
     lineadj $ln $lo
     lo=$ln
+    sleep 1
 done
-echo
+echo -e "\e[?25h"
 echo "ready!"
-
-
