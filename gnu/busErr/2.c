d0c6a8c9beedbf0c5aed2460cedc46aeebcd4d54
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-04-23 16:42:45 +0800

    add busErr example

---
+++ gnu/busErr/2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h> /* O_ constants */
+#include <unistd.h> /* ftruncate */
+#include <sys/mman.h> /* mmap */
+
+int main()
+{
+    int fd;
+    int *map;
+    int size = sizeof(int);
+    long pgSize= sysconf(_SC_PAGESIZE);
+
+    char *name = "/a";
+
+    printf("pagesize:%x\n",pgSize);
+
+    shm_unlink(name);
+    fd = shm_open(name, O_RDWR | O_CREAT, (mode_t)0600);
+    /* THIS is the cause of the problem. */
+
+    /*ftruncate(fd, size);*/	    //问题点,enable 运行正常
+
+    map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    /* This is what generates the SIGBUS. */
+    *map = 0;
+}
