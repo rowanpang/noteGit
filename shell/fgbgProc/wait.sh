03c0d55e21581b90c1b25df06f0e4c69cbaddd60
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-05-10 11:38:46 +0800

    add wait usage exp;

--- shell/fgbgProc/wait.sh
+++ shell/fgbgProc/wait.sh
@@ -12,12 +12,41 @@ function now(){
     echo `date +%Y%m%d-%H%M%S`
 }
 
-echo "start:$(now)"
+function waitn() {
+    echo "waitn start:$(now)"
 
-sleep5 &
-sleep10 &
+    sleep5 &
+    pid1=$!
+    sleep10 &
+    pid2=$!
+    echo "waiting $pid1,$pid2---"
+    wait -n $pid1 $pid2			#pid1退出,wait就返回
+    echo "wait $pid1,$pid2: $?"
+    wait -n $pid1 $pid2			#虽然pid1 not exist,但pid2 exist,所以可以正常wait
+    echo "wait2 $pid1,$pid2: $?"
 
-echo "waiting---"
-wait
+    echo "sleep 10"
+    sleep 10
+    wait -n $pid2			#此时pid2已经退出,wait失败, 返回127
+    echo "wait $pid2: $?"
 
-echo "end:$(now)"
+    echo "end:$(now)"
+}
+
+function waitjust() {
+    echo "waitjust start:$(now)"
+
+    sleep5 &
+    pid1=$!
+    sleep10 &
+    pid2=$!
+    echo "waiting $pid1,$pid2---"
+    wait				#pid1,pid2都退出后才返回.
+    echo "wait $?"			#alway return 0;
+
+    echo "end:$(now)"
+}
+
+waitn
+echo
+waitjust
