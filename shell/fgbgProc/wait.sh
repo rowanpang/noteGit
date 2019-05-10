a808b1a1336fd4bdd4668a6d71b06915561ae31a
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-05-10 13:58:37 +0800

    verbose wait usage

--- shell/fgbgProc/wait.sh
+++ shell/fgbgProc/wait.sh
@@ -8,8 +8,30 @@ function sleep10(){
     sleep 10
 }
 
+function sleep30(){
+    sleep 30
+}
+
 function now(){
     echo `date +%Y%m%d-%H%M%S`
+}
+
+function waitpid() {
+    echo "waitpid start:$(now)"
+    sleep5 &
+    pid1=$!
+    sleep10 &
+    pid2=$!
+    sleep30 &
+    pid3=$!
+
+    echo "waiting $pid1,$pid2---"
+    wait $pid1 $pid2			#pid1,pid2都退出,wait就返回
+    echo "wait $pid1,$pid2: $?"
+    echo "waitpid end:$(now)"
+
+    wait
+    echo "waitpid end-2:$(now)"
 }
 
 function waitn() {
@@ -21,16 +43,16 @@ function waitn() {
     pid2=$!
     echo "waiting $pid1,$pid2---"
     wait -n $pid1 $pid2			#pid1退出,wait就返回
-    echo "wait $pid1,$pid2: $?"
+    echo "wait $pid1,$pid2 end: $?,$(now)"
     wait -n $pid1 $pid2			#虽然pid1 not exist,但pid2 exist,所以可以正常wait
-    echo "wait2 $pid1,$pid2: $?"
+    echo "wait $pid1,$pid2 end-2: $?,$(now)"
 
     echo "sleep 10"
     sleep 10
     wait -n $pid2			#此时pid2已经退出,wait失败, 返回127
     echo "wait $pid2: $?"
 
-    echo "end:$(now)"
+    echo "waitn end:$(now)"
 }
 
 function waitjust() {
@@ -47,6 +69,8 @@ function waitjust() {
     echo "end:$(now)"
 }
 
+waitpid
+echo
 waitn
 echo
 waitjust
