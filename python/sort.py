1055f65514f3a26f060bd26c8b390b776b73db95
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-08-13 14:46:45 +0800

    sort example update

--- python/sort.py
+++ python/sort.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 # sort 比较的是obj.inter()得到的list.
 # 所以，对collections sorted 的返回结果为key ref ./collections.py
-L = [('b',4),('a',3),('c',1),('d',2)]
+L = [('b',3),('a',4),('c',2),('d',1)]
 print type(L)
 for l in L:
     print l
     print type(l)
+
+print "key:-----"
 print sorted(L,key=lambda x:x[0])
 print sorted(L,key=lambda x:x[1])
 
-L = ['a','c','b','d']
+#L = ['a','c','b','d']
 # print sorted(L,cmp=lambda x,y:cmp(x[1],y[1]))   #error
+
+print "cmp:-----"
 print sorted(L,cmp=lambda x,y:cmp(x[0],y[0]))   #error
+print sorted(L,cmp=lambda x,y:cmp(x[1],y[1]))   #error
