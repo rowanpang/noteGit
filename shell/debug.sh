cb9001ae174f7a481ad922d620e9b3f5e2920561
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-11-07 12:53:25 +0800

    fix bugs for ref $1

--- shell/debug.sh
+++ shell/debug.sh
@@ -2,29 +2,29 @@
 
 verbose="3"
 function pr_debug(){
-    fmt=$1 && shift
+    fmt="$1" && shift
     [ $verbose -ge 7 ] && printf "$fmt\n" "$@"
 }
 
 function pr_info(){
-    fmt=$1 && shift
+    fmt="$1" && shift
     [ $verbose -ge 6 ] && printf "$fmt\n" "$@"
 }
 
 function pr_notice(){
-    fmt=$1 && shift
+    fmt="$1" && shift
     [ $verbose -ge 5 ] && printf "$fmt\n" "$@"
 }
 
 function pr_warn(){
     #33m,yellow
-    fmt=$1 && shift
+    fmt="$1" && shift
     [ $verbose -ge 4 ] && printf "\033[1;33m""WARNING! $fmt\n""\033[0m" "$@"
 }
 
 function pr_err(){
     #31m,red
-    fmt=$1 && shift
+    fmt="$1" && shift
     [ $verbose -ge 3 ] && printf "\033[1;31m""ERROR! $fmt,Exit -1!\n""\033[0m" "$@"
     exit -1
 }
@@ -42,8 +42,8 @@ function testMain(){
     verbose=7
     echo "----start"
     pr_debug	"this is debug"
-    pr_info	"this is info"
-    pr_notice	"this is notice"
+    pr_info	"this is info: %5s" "adb"
+    pr_notice	"this is notice:----%s" "kab---ab---ab"
     pr_warn	"this is warn: %s,%3d\n" "warning" "10"
     pr_err	"this is err: %s" "kkkkk"
 
