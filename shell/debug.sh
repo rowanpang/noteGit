90242dfc13c394eb4afe2d2ec406617d2eed7b58
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-11-07 13:02:05 +0800

    printf -- "-----nxgzip loop %2d: 12:56:15\n" 1 fix for start with -

--- shell/debug.sh
+++ shell/debug.sh
@@ -3,29 +3,29 @@
 verbose="3"
 function pr_debug(){
     fmt="$1" && shift
-    [ $verbose -ge 7 ] && printf "$fmt\n" "$@"
+    [ $verbose -ge 7 ] && printf -- "$fmt\n" "$@"
 }
 
 function pr_info(){
     fmt="$1" && shift
-    [ $verbose -ge 6 ] && printf "$fmt\n" "$@"
+    [ $verbose -ge 6 ] && printf -- "$fmt\n" "$@"
 }
 
 function pr_notice(){
     fmt="$1" && shift
-    [ $verbose -ge 5 ] && printf "$fmt\n" "$@"
+    [ $verbose -ge 5 ] && printf -- "$fmt\n" "$@"
 }
 
 function pr_warn(){
     #33m,yellow
     fmt="$1" && shift
-    [ $verbose -ge 4 ] && printf "\033[1;33m""WARNING! $fmt\n""\033[0m" "$@"
+    [ $verbose -ge 4 ] && printf -- "\033[1;33m""WARNING! $fmt\n""\033[0m" "$@"
 }
 
 function pr_err(){
     #31m,red
     fmt="$1" && shift
-    [ $verbose -ge 3 ] && printf "\033[1;31m""ERROR! $fmt,Exit -1!\n""\033[0m" "$@"
+    [ $verbose -ge 3 ] && printf -- "\033[1;31m""ERROR! $fmt,Exit -1!\n""\033[0m" "$@"
     exit -1
 }
 
