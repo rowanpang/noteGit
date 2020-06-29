a091aec76912cd1aa78b441131cd72a7495bea8e
Rowan Pang <pangweizhen.2008@qq.com>
2020-06-29 09:52:22 +0800

    updat posxi file lock

--- fs/locks/posix.c
+++ fs/locks/posix.c
@@ -35,10 +35,11 @@ int main(int argc, char const *argv[])
 
     fd = open(FILENAME,O_RDWR | O_CREAT,644);
 
+    //getlk 相同的地方
     fltmp.l_type = F_RDLCK;
     fltmp.l_start = 0;
     fltmp.l_whence = SEEK_SET;
-    fltmp.l_len = 5;
+    fltmp.l_len = 1;
 
     ret = fcntl(fd,F_GETLK,&fltmp);
     if (ret !=0 ){
@@ -53,7 +54,26 @@ int main(int argc, char const *argv[])
 	 */
     }
 
-    if(write_lock(fd,0,SEEK_SET,0) < 0) {
+    //getlk 不同的地方
+    fltmp.l_type = F_RDLCK;
+    fltmp.l_start = 5;
+    fltmp.l_whence = SEEK_SET;
+    fltmp.l_len = 1;
+
+    ret = fcntl(fd,F_GETLK,&fltmp);
+    if (ret !=0 ){
+	printf("fcntl getlk error:%d,%s\n",errno,strerror(errno));
+	goto OUT;
+    } else {
+	printf("fcntl getlk success flock ret:%d\n",fltmp.l_type);
+	/*
+	 *#define F_RDLCK         0
+	 *#define F_WRLCK         1
+	 *#define F_UNLCK         2
+	 */
+    }
+
+    if(write_lock(fd,0,SEEK_SET,5) < 0) {
 	if(errno == EACCES || errno == EAGAIN){	//EAGAIN == EWOULDBLOCK
 	    printf("unable to lock %s,is %s already running?\n", FILENAME,argv[0]);
 	    goto OUT;
