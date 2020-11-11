f66f541086db7f00823a7ab269572fd936d2c55d
Rowan Pang <pangweizhen.2008@qq.com>
2020-11-11 14:45:06 +0800

    cap dac will droped in system()

--- security/capability/user/main.c
+++ security/capability/user/main.c
@@ -4,50 +4,72 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
-int fileline(void)
+int fileread(void)
 {
-	FILE *fp = NULL;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read = 0;
+    FILE *fp = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
 
-	fp = fopen("./test.txt","r");
-	if(fp == NULL){
-		perror("\033[0;31mwhen open test.txt");
-		printf("\033[0m");
-		exit(EXIT_FAILURE);
-	}
+    fp = fopen("./test.txt","r");
+    if(fp == NULL){
+        perror("\033[0;31mwhen open test.txt");
+        printf("\033[0m");
+        exit(EXIT_FAILURE);
+    }
 
-	while((read = getline(&line,&len,fp)) != -1){
-		line[read-1] = '\0';
-		printf("read:%s,len:%zu\n",line,read);
-	}
+    while((read = getline(&line,&len,fp)) != -1){
+        line[read-1] = '\0';
+        printf("read:%s,len:%zu\n",line,read);
+    }
 
-	if(line)
-		free(line);
+    if(line)
+        free(line);
 
-	if(fp)
-		fclose(fp);
-	return 0;
+    if(fp)
+        fclose(fp);
+    return 0;
+}
+
+int capdac()
+{
+    int fd;
+    char *fpath="/root/capdat.test.txt";
+    char cmd[256]={0};
+    sprintf(cmd,"echo kkk > %s",fpath);
+    printf("syscmd: %s",cmd);
+    system(cmd);
+
+    fd = open(fpath,O_RDWR|O_CREAT|O_APPEND);
+    write(fd,cmd,strlen(cmd));
+    close(fd);
+}
+
+int capsuid()
+{
+    if(setuid(74) == -1){
+        perror("\033[0;31mwhen setuid(74)");
+        printf("\033[0m");
+    }else{
+        printf("after setuid(74)");
+        printf("uid:%d,euid:%d\n",getuid(),geteuid());
+        printf("try read file 2\n");
+        fileread();
+    }
 }
 
 int main(int argc,char** argv)
 {
+    printf("uid:%d,euid:%d\n",getuid(),geteuid());
+    printf("try read file 1\n");
+    fileread();
 
-	printf("try read file 1\n");
-	fileline();	
-	printf("uid:%d,euid:%d\n",getuid(),geteuid());
-	if(setuid(74) == -1){
-		perror("\033[0;31mwhen setuid(74)");
-		printf("\033[0m");
-	}else{
-		printf("after setuid(74)");
-		printf("uid:%d,euid:%d\n",getuid(),geteuid());
-		printf("try read file 2\n");
-		fileline();
-	}
+    capdac();
 
-	return 0;
+    return 0;
 }
-	
+
