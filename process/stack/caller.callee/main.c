2ba92d5e2bade97a6e00f6831ccee4a9b3961919
Rowan Pang <pangweizhen.2008@qq.com>
2021-07-30 15:04:25 +0800

    stack example

--- process/stack/caller.callee/main.c
+++ process/stack/caller.callee/main.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int myadd9(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9)\
+{
+    int sum;
+    sum = a1+a2+a3+a4+a5+a6+a7+a8+a9;
+
+    return sum;
+}
+
 int myadd(int a, int b)
 {
-	int c;
-	int d;
+    int c;
+    int d;
 
-	c = random();
+    c = random();
 
-	d = a + b + c;
-	return d;
+    d = a + b + c;
+    return d;
 }
 
 int main()
 {
-	int a;
-	int b;
+    int a;
+    int b;
+    int aa1,aa2,aa3,aa4,aa5,aa6,aa7,aa8,aa9;
+    int sum,sum1,sum2;
 
-	int sum,sum1;
+    a = random();
+    b = random();
+    sum = a + b;
+    sum1 = myadd(a,b);
 
-	a = random();
-	b = random();
+    aa1 = random();
+    aa2 = random();
+    aa3 = random();
+    aa4 = random();
+    aa5 = random();
+    aa6 = random();
+    aa7 = random();
+    aa8 = random();
+    aa9 = random();
 
-	sum = a + b;
+    sum2 = myadd9(aa1,aa2,aa3,aa4,aa5,aa6,aa7,aa8,aa9);
 
-	sum1 = myadd(a,b);
-
-	return 0;
+    return 0;
 }
 
 
