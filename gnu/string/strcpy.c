61d810e7f1093e4a9bc4a8e5edca508514fc8fd4
Rowan Pang <pangweizhen.2008@qq.com>
2021-09-02 18:12:55 +0800

    dir order

--- gnu/string/strcpy.c
+++ gnu/string/strcpy.c
@@ -6,6 +6,13 @@ int main(int argc,char** argv)
     char *src="pangwz";
     char dst[10]={0};
 
+    char *src2="123";
+
+    src="kkkk";
+    printf("%02x,%02x,%02x\n",'1',src2[0],src2[1]);
+    printf("%02x,%02x,%02x\n",'1',*src2++);
+    printf("%02x,%02x,%02x\n",'1',*src2++);
+
     strcpy(dst,src);
 
     printf("after cpy %s \n",dst);
