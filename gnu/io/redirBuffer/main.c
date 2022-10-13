041477d17e6aa9af4c2b70a9b889b8e18e56d052
Rowan Pang <pangweizhen.2008@qq.com>
2022-10-13 11:21:36 +0800

    BUFSIZ show

--- gnu/io/redirBuffer/main.c
+++ gnu/io/redirBuffer/main.c
@@ -6,30 +6,39 @@
 
 int main (int argc, char **argv)
 {
+    printf("glibc BUFSIZ:%d\n",BUFSIZ);
+    fflush(stdout);
     switch (argc) {
         case 2:
+            printf("glibc set _IOLBF");
+            fflush(stdout);
 	    setvbuf(stdout,NULL,_IOLBF,0);
 	    break;
         case 3:
+            printf("glibc set _IONBF");
 	    setvbuf(stdout,NULL,_IONBF,0);
 	    break;
         default:
 	    break;
     }
 
-    write(STDOUT_FILENO,"------write with \\n---\n",30);
-    write(STDOUT_FILENO,"------write withou \\n--------",30);
-    write(STDOUT_FILENO,"------write 2--------\n",30);
+    write(STDOUT_FILENO,"---------write syscall func---\n",20);
 
-    printf ("-------print:line buffered out\\n----\n");
-    fwrite("--------fwrite:stdout \\n------\n",40,1,stdout);
-    printf ("-------print:without \\n,----");
-    fwrite("--------fwrite:stdout 1------",40,1,stdout);
+    write(STDOUT_FILENO,"---------write with \\n---\n",20);
+    write(STDOUT_FILENO,"---------write withou \\n--------",20);
+    write(STDOUT_FILENO,"---------write 2--------\n",20);
 
-    fwrite("--------fwrite stderr 1------",40,1,stderr);
-    fwrite("--------fwrite stderr \\n------\n",40,1,stderr);
+    write(STDOUT_FILENO,"---------glibc APIs-----\n",20);
+    printf("printf:line buffered out\\n----\n");
+    fwrite("fwrite:stdout \\n------\n",20,1,stdout);
+    printf("printf:without \\n,----");
+    fwrite("fwrite:stdout 1------",20,1,stdout);
+
+    fwrite("fwrite stderr 1------",20,1,stderr);
+    fwrite("fwrite stderr \\n------\n",20,1,stderr);
 
 
     sleep(3);
+    fflush(stdout);
     return 0;
 }
